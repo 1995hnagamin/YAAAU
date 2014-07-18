ff844ae4dcdc838282c85db7490802588c57df15
hnagamin <1995.hnagamin@gmail.com>
2014-07-18 19:02:28 +0900

    いくつかのDecoratorを追加

--- SpinachConfigParser.py
+++ SpinachConfigParser.py
@@ -2,19 +2,6 @@ import ConfigParser
 import os.path
 import getpass
 
-def prompt(message):
-    def func():
-        print message,
-        return raw_input()
-
-    return func
-
-def pass_prompt(message):
-    def func():
-        return getpass.getpass(prompt=message)
-    
-    return func
-
 
 class SpinachConfigParser:
     def __init__(self, filepath):
@@ -34,3 +21,59 @@ class SpinachConfigParser:
     def getpass(self, section, option, message):
         return self.get(section, option, pass_prompt(message))
 
+
+class SpinachDecorator:
+    def __init__(self, config, message, validate):
+        self.config = config
+        self.message = message
+        self.validate = validate
+
+    def get(self, section, option):
+        if self.config.has_option(section, option):
+            return self.config.get(section, option)
+        else:
+            print self.message,
+            value = raw_input()
+            if valudate(value):
+                return value
+            else:
+                return self.get(section, option)
+
+    def has_option(self, section, opstion):
+        return self.has_option(section, option)
+
+
+class DefaultDecorator:
+    def __init__(self, config, dictionary):
+        self.config = config
+        self.defaults = dictionary
+
+    def get(self, section, option):
+        if self.config.has_option(section, option):
+            return self.config.get(section, option)
+        elif (section, option) in self.defaults:
+            return self.defaults[ (section, option) ]
+        else:
+            raise ConfigParser.NoOptionError
+
+    def has_option(self, section, option):
+        return self.defaults.has_key( (section, option) ) or self.config.has_key(section, option)
+
+
+class PasswordDecorator:
+    def __init__(self, config, dictionary, message, validate):
+        self.config = config
+        self.need_to_hide = dictionary
+        self.message = message
+        self.validate = validate
+
+    def get(self, section, option):
+        if self.config.has_option(section, option):
+            return self.config.get(section, option)
+        elif (section, option) in self.need_to_hide:
+            return getpass.getpass(prompt=self.message)
+        else:
+            raise ConfigParser.NoOptionError
+
+    def has_option(self, section, option):
+        return 
